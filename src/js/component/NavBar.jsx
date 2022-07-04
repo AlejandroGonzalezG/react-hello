8c2083bb87de0f186f93e3ee25926dd00e322fe7
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-07-04 16:07:28 +0000

    avance navbar y jumbotron

---
+++ src/js/component/NavBar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropType from "prop-types";
+
+
+const mySuperStyles = {
+	fontSize: "16px",
+
+};
+
+const NavBar = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="#" >Home</a>
+                        <a className="nav-link" href="#">About</a>
+                        <a className="nav-link" href="#">Services</a>
+                        <a className="nav-link" href="#">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+	);
+};
+
+
+
+export default NavBar;
+
+
+
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Jumbotron from "./Jumbotron.jsx";
+import NavBar from "./NavBar.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<NavBar />
+			<Jumbotron title="A Warm Welcome!" description="Lorem pisum dolor sit amet, consectetur adpisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscpit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+					   buttonLabel="Call the action" buttonURL="https://4geeks.com/es" />
 		</div>
 	);
 };
