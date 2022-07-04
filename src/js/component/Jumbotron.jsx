d8b491558c3f54975081063b34dbdd76ef88ef7f
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-07-04 16:37:58 +0000

    avance landing page

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
+const estiloJumbotron = {
+	background: "#F1F2F2",
+};
+
+const estiloBoton = {
+	padding: "10px",
+    marginTop: "20px",
+    marginBottom: "80px",
+
+};
+
 const Jumbotron = (props) => {
 	return (
-		<div className="jumbotron m-5">
-			<h1 className="display-4">{props.title}</h1>
-			<p className="lead">{props.description}</p>
-			<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">{props.buttonLabel}</a>
-		</div>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+		            <div className="jumbotron p-4" style={estiloJumbotron}>
+			            <h1 className="display-4 text-start">{props.title}</h1>
+			            <p className="lead text-start">{props.description}</p>
+			            <a className="btn btn-primary btn-lg" href={props.buttonURL} role="button" style={estiloBoton}>{props.buttonLabel}</a>
+		            </div>
+                </div>
+            </div>
+        </div>
 	)
 };
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,7 +7,7 @@ import NavBar from "./NavBar.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
+		<div className="divPrincipal">
 			<NavBar />
 			<Jumbotron title="A Warm Welcome!" description="Lorem pisum dolor sit amet, consectetur adpisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscpit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 					   buttonLabel="Call the action" buttonURL="https://4geeks.com/es" />
